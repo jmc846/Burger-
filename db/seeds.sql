18e96f3bd5dad8d0d80ca56734fff29d6d146d79
Jonathan Cobb <jmc846@scarletmail.rutgers.edu>
2020-08-27 11:26:22 -0400

    same

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
 -- In this file, write insert queries to populate the `burgers` table with at least three entries.
-INSERT INTO(burger) VALUES ('chicken burger') ;
-INSERT INTO(burger)  VALUES ('buffalo blue cheese burger');
-INSERT INTO (burger)  VALUES ('turkey avocado burger') ;
+INSERT INTO burgers (name) VALUES ('chicken burger');
+INSERT INTO burgers (name) VALUES ('buffalo blue cheese burger');
+INSERT INTO burgers (name) VALUES ('turkey avocado burger') ;
