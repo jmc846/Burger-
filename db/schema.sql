ba5bd9863a5299b3ccd6c1f277c5350c7b1cee53
Jonathan Cobb <jmc846@scarletmail.rutgers.edu>
2020-08-27 10:27:03 -0400

    added inserts to seeds and db data, created dependencies

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,13 @@
- * Create the `burgers_db`.
-   * Switch to or use the `burgers_db`.
-   * Create a `burgers` table with these fields:
-     * **id**: an auto incrementing int that serves as the primary key.
-     * **burger_name**: a string.
-     * **devoured**: a boolean.
+DROP DATABASE IF EXISTS
+
+CREATE DATABASE burgers_db;
+
+USE burger_db;
+
+CREATE TABLE BURGER (
+  id Int( 11 ) AUTO_INCREMENT NOT NULL;
+  burger_name VARCHAR(30 ) NOT NULL;
+  devoured ;
+
+  PRIMARY KEY (id) ;
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,6 @@
-In this file, write insert queries to populate the `burgers` table with at least three entries.
+-- In this file, write insert queries to populate the `burgers` table with at least three entries.
+
+
+INSERT INTO burgers (chicken burger) VALUES ();
+INSERT INTO burgers (buffalo blue cheese burger) VALUES ();
+INSERT INTO burgers (turkey avocado burger) VALUES ();
