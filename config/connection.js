46cbca027372a10f39adb2794fecd2196928b41f
Jonathan Cobb <jmc846@scarletmail.rutgers.edu>
2020-08-30 15:20:07 -0400

    updated dependencies

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,6 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var jawsSql= process.env.JAWSDB_URL;
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -8,16 +9,13 @@ var connection = mysql.createConnection({
     password: "67504@jc",
     database: "burgers_db"
   });
-  
+
   // Make connection.
-  connection.connect(function(err) {
-    if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
-    }
-    console.log("connected as id " + connection.threadId);
-  });
   
+  
+  connection= mysql.createConnection(jawsSql);
+  
+  console.log(jawsSql)
   
   // Export connection for our ORM to use.
   module.exports = connection;
