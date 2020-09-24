ecd6453b954ee2db1297766719e698d085f3cb17
Jonathan Cobb <jmc846@scarletmail.rutgers.edu>
2020-09-24 00:31:06 -0400

    dotenv changes

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,12 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-
+require("dotenv").config()
 var connection = mysql.createConnection(process.env.JAWSDB_URL ||{
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "67504@jc",
-    database: "burgers_db"
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
 
   });
   // if (typeof jawsSql ===undefined || jawsSql ===00)
