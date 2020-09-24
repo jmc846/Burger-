8b6d4ae9d5211f78d5779e7b9a79da7916da11b7
Jonathan Cobb <jmc846@scarletmail.rutgers.edu>
2020-09-24 00:08:55 -0400

    fixing heroku deploy issue

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,7 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-var jawsSql= process.env.JAWSDB_URL;
 
-var connection = mysql.createConnection({
+var connection = mysql.createConnection(process.env.JAWSDB_URL ||{
     host: "localhost",
     port: 3306,
     user: "root",
@@ -10,14 +9,14 @@ var connection = mysql.createConnection({
     database: "burgers_db"
 
   });
-  if (typeof jawsSql ===undefined || jawsSql ===00)
+  // if (typeof jawsSql ===undefined || jawsSql ===00)
 
   // Make connection.
   
   
-  connection= mysql.createConnection(jawsSql);
+  // connection= mysql.createConnection(jawsSql);
   
-  console.log(jawsSql)
+  // console.log(jawsSql)
   
   // Export connection for our ORM to use.
   module.exports = connection;
